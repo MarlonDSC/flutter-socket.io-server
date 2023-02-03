18e9adf3750121a1932b61869e8fde92906441e5
MarlonDSC <117086700+MarlonSubuyu@users.noreply.github.com>
2023-02-03 13:51:05 -0400

    fix deployment

--- index.js
+++ index.js
@@ -5,34 +5,25 @@ require('dotenv').config();
 // DB Config
 require('./database/config').dbConnection();
 
-
 // App de Express
 const app = express();
 
 // Lectura y parseo del Body
 app.use( express.json() );
 
-
 // Node Server
 const server = require('http').createServer(app);
 module.exports.io = require('socket.io')(server);
 require('./sockets/socket');
 
-
-
-
 // Path público
 const publicPath = path.resolve( __dirname, 'public' );
 app.use( express.static( publicPath ) );
-
-
 
 // Mis Rutas
 app.use( '/api/login', require('./routes/auth') );
 app.use( '/api/usuarios', require('./routes/usuarios') );
 app.use( '/api/mensajes', require('./routes/mensajes') );
-
-
 
 server.listen( process.env.PORT, ( err ) => {
 
